56ae35459491674b9c9de6f12ed7f7d109b0f575
Sylvia Nkosi <synkojhb025@studnet.wethinkcode.co.za>
2025-11-02 19:24:20 +0200

    py password validator

--- PASSWORD-VALIDATOR/PasswordValidator.py
+++ PASSWORD-VALIDATOR/PasswordValidator.py
@@ -1,4 +1,27 @@
 # password Validator
+def password_validator(string):
+    lower = 0
+    upper = 0
+    num = 0
+    special = 0
+    
+    length = len(string)
 
-def password_validator():
-    pass
+    for char in string:
+        if char.isupper():
+            upper += 1
+        elif char.islower():
+            lower += 1
+        elif char.isdigit():
+            num += 1
+        elif not char.islower() and not char.isupper() and not char.isdigit() and char not in ['-']:
+            special += 1
+
+
+    if upper > 0 and lower > 0 and num > 0 and special > 0 and length > 8:
+        return True
+    else:
+        return False
+    
+    
+

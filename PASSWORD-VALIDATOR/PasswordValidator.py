52ce8c8ec633e696834d3e5c97c9a082c01495ac
Sylvia Nkosi <synkojhb025@studnet.wethinkcode.co.za>
2025-11-02 20:41:12 +0200

    update on rules

--- PASSWORD-VALIDATOR/PasswordValidator.py
+++ PASSWORD-VALIDATOR/PasswordValidator.py
@@ -1,10 +1,18 @@
 # password Validator
+# password must be > greater than 8 characters in length
+# must contain atleast 1 lowercase
+# atleast on uppercase
+# atleast on digit
+# and atleast one special character excluding -, will not be counted as special character
+# password must not have - and spaces
+
 def password_validator(string):
     lower = 0
     upper = 0
     num = 0
     special = 0
-    
+    exclusion = 0
+
     length = len(string)
 
     for char in string:
@@ -14,14 +22,26 @@ def password_validator(string):
             lower += 1
         elif char.isdigit():
             num += 1
-        elif not char.islower() and not char.isupper() and not char.isdigit() and char not in ['-']:
+        elif not char.islower() and not char.isupper() and not char.isdigit():
             special += 1
+        elif char == '-' or char ==" ":
+            exclusion += 1
 
 
-    if upper > 0 and lower > 0 and num > 0 and special > 0 and length > 8:
+    print("this is lower", lower)
+    print("this is upper", upper)
+    print("this is num", num)
+    print("this is special", special)
+    print("this is exclustion", exclusion)
+
+
+
+    if upper > 0 and lower > 0 and num > 0 and special > 0 and length > 8 and exclusion < 1:
         return True
     else:
         return False
+    
+print(password_validator("SYL5@-viatn"))
     
     
 

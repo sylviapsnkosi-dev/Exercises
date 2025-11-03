b02858550c60dc93cf28bf06a3510de89eff723d
Sylvia Nkosi <synkojhb025@studnet.wethinkcode.co.za>
2025-11-03 22:47:37 +0200

    working version with notes

--- PASSWORD-VALIDATOR/passwordValidator.java
+++ PASSWORD-VALIDATOR/passwordValidator.java
@@ -1,11 +1,10 @@
 public class PasswordValidator{
-
     public static void main(String[] args){
-    
-        //call the method
-        isValidPassword("mypassword");
 
-        
+        //call the method
+        String result = isValidPassword("mynaSG$12&");
+        System.out.println(result);
+
     }
 
     static String isValidPassword(String password){
@@ -14,7 +13,7 @@ public class PasswordValidator{
         int lower = 0;
         int num = 0;
         int spec = 0;
-
+    
 
         //1 the length of the password must be greater than 8
         int size = password.length();
@@ -33,19 +32,30 @@ public class PasswordValidator{
             }else if(Character.isDigit(i)){
                 num += 1;
 
-            }else if ((!Character.isLetter(i))&&(Character.isDigit(i))){
+            }else if ((!Character.isLetter(i))&&(!Character.isDigit(i))){
                 spec += 1; 
             }
         }
 
         // now i use an if statement to determine whether is the password contains atleast on of (lower letter, uppercase letter, number, special character and length > 8)
-        if (upper > 0 && lower > 0 && num > 0 && spec > 0 && size > 8){
-            return "password valid";
+        if ((upper > 0) && (lower > 0) && (num > 0) && (spec > 0) && (size >= 8)){
+            return "password is valid";
+            
         }else {
-            return "false";
+            return "invalid password";
+        
         }
 
-    }
-
-    
+    }   
 }
+
+/*
+NOTES
+1. when we want to see the result or how the function behave when we call it in the main class -- 
+    ensure to create a variable that holds the results of the class - and print this variable
+
+2. in py we can use not in [ list ], to exclude characters from this list in JAVA
+    (!nameoflist.of(items of the list).contains(pass in character))
+
+*/ 
+
